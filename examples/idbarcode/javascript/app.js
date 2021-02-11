a3240c7729777d5b51503125219fca0db6d7d5b5
Vjekoslav Artic <vjekoslav.artic@microblink.com>
2021-02-11 13:24:40 +0100

    Updated example license keys

--- examples/idbarcode/javascript/app.js
+++ examples/idbarcode/javascript/app.js
@@ -30,7 +30,7 @@ function main()
     }
 
     // 1. It's possible to obtain a free trial license key on microblink.com
-    let licenseKey = "sRwAAAYJbG9jYWxob3N0r/lOPk4/w35CpJlWLIUTxZO6heZwZ0ieAgWPdJbjL234+8kyLov9AmMYZU27gBbCKJ44GF7LxEO0L7YeysCJvf8VXD93eduMRFMwASjo7bwKCdcLY10azKaUKGfFors3peLh8cJ3xdpMZJB4NKxahGsq+V/jGmmjGt6m0YM+HWHXuZ5H9WyTJELTZrc9DVgUzjZLam3MaIcObAaOcQaGZELchW4E78RTBiL1IeU5rBE9A5fqRzvfeVsIEbMwHoESFtfhMiTk72KXDc+qyQ494NOiiAMUJ31XpolzxPIlpm3b8eIUhQrgaeLbDeEHV4h5BIdoQKzv";
+    let licenseKey = "sRwAAAYJbG9jYWxob3N0r/lOPk4/w35CpJlWLC04YOZP6o4omehP4Kw61GsKVR5+hnhdmJM0Jl3uPIjDdf2G2jTObySnuUJybl5Gtls64/fJrq74wM7tQj9fdN+uaOcwKw6bTeO0MAXABpAgPju+JcPnlsbqm4mKIN/BQ4TX+FV5MPQB/NotnGYBzdk3jaf+D1YnJcEuTgAs7Oz8bY58MKSxAFSBntni8hznFa9neQypvnBrPE2IA7p0hDNewzyVD7QLU8t2/4Q535Alm80JR3xFT+9qLRraWUb85cKEDSE1PF/rT/0CJE3J5XiJWBBbgL43ioliTlfpP9hfAylphEwtazUU3Io=";
 
     if ( window.location.hostname === "blinkid.github.io" )
     {
